1cfbcc50d2c1f16f6273813f6644c54bc326e548
Aman Shyam Verma <92292381+wizard-aka-aman@users.noreply.github.com>
2024-09-12 22:21:33 +0530

    Update Register.jsx

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/register",
+        "https://job-portal-backend-uz7u.onrender.com/api/v1/user/register",
         { name, phone, email, role, password },
         {
           headers: {
