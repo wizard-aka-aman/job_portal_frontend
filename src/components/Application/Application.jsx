b9a28dd05827193e0b0a6af60066810a21ec4dd2
Aman Shyam Verma <92292381+wizard-aka-aman@users.noreply.github.com>
2024-09-12 22:07:41 +0530

    Update Application.jsx

--- src/components/Application/Application.jsx
+++ src/components/Application/Application.jsx
@@ -35,7 +35,7 @@ const Application = () => {
     formData.append("jobId" , id);
 
     try {
-      const {data} = await axios.post("http://localhost:4000/api/v1/application/post" ,formData, {withCredentials:true,
+      const {data} = await axios.post("https://job-portal-backend-uz7u.onrender.com/api/v1/application/post" ,formData, {withCredentials:true,
         headers: {'Content-Type': 'multipart/form-data' }
       });
       setName("");
